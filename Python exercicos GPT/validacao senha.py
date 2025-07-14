625197e3e38be3e007003407fe5184f24e90aeca
Thaynacosta25 <115906621+Thaynacosta25@users.noreply.github.com>
2025-07-14 16:53:11 +0800

    Delete Python exercicos GPT directory

--- Python exercicos GPT/validacao senha.py
+++
@@ -1,9 +0,0 @@
-senha_correta = 1234
-
-senha = ""
-
-while senha != senha_correta:
-    senha = int(input("Digite sua senha: ") )
-    if senha != senha_correta:
-        print("Senha incorreta. Tente novamente! ")
-print("Acesso liberado!")
